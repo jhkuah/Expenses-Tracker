45b564d08cbc58c0bd3faff2d5842d0dc3d693b5
jhkuah <122422751+jhkuah@users.noreply.github.com>
2023-06-08 19:27:19 +0800

    Edited button text

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -85,7 +85,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__action">
-        <button onClick={props.onCancel}>Cancel</button>
+        <button onClick={props.onCancel}>Cancel This</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
